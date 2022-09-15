ec8ef891d3c4d53a22fafb30c535847cc5403bd1
kureungkureung <terry9611@naver.com>
2022-09-15 21:18:35 +0900

    feat: 공 숫자 생성자 구현

--- src/main/java/io/kureung/BallNumber.java
+++ src/main/java/io/kureung/BallNumber.java
@@ -1,5 +1,7 @@
 package io.kureung;
 
+import java.util.Arrays;
+
 public enum BallNumber {
     ONE(1),
     TWO(2),
@@ -15,6 +17,13 @@ public enum BallNumber {
 
     BallNumber(int number) {
         this.number = number;
+    }
+
+    public static BallNumber from(int numberValue) {
+        return Arrays.stream(values())
+                .filter(ballNumber -> ballNumber.number == numberValue)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("공의_숫자는 1 이상 9이하이어야 합니다."));
     }
 
     public int number() {

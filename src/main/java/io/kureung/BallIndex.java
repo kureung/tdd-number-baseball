10bab5bbfece20419e0aaefea7b41dfd49bd3ff4
kureungkureung <terry9611@naver.com>
2022-09-15 21:11:15 +0900

    refactor: 공 인덱스 생성자 구현

--- src/main/java/io/kureung/BallIndex.java
+++ src/main/java/io/kureung/BallIndex.java
@@ -1,7 +1,22 @@
 package io.kureung;
 
+import java.util.Arrays;
+
 public enum BallIndex {
-    ONE,
-    TWO,
-    THREE;
+    ONE(1),
+    TWO(2),
+    THREE(3);
+
+    private final int indexValue;
+
+    BallIndex(int indexValue) {
+        this.indexValue = indexValue;
+    }
+
+    public static BallIndex from(int indexValue) {
+        return Arrays.stream(values())
+                .filter(ballIndex -> ballIndex.indexValue == indexValue)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("공 인덱스는 1 이상 3 이하 입니다."));
+    }
 }

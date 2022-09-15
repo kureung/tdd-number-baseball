f9b483691373dd64aeae1c66b90064864c69581c
kureungkureung <terry9611@naver.com>
2022-09-15 22:01:45 +0900

    feat: 볼 생성자 구현

---
+++ src/main/java/io/kureung/Balls.java
@@ -0,0 +1,25 @@
+package io.kureung;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Balls {
+
+    private static final int ONE = 1;
+
+    private final List<Ball> balls;
+
+    private Balls(List<Ball> balls) {
+        this.balls = balls;
+    }
+
+    public static Balls from(NumbersGenerator numbersGenerator) {
+        List<Integer> numbers = numbersGenerator.numbers();
+        List<Ball> balls = IntStream.rangeClosed(BallIndex.ONE.indexValue(), BallIndex.THREE.indexValue())
+                .boxed()
+                .map(ballIndex -> new Ball(BallIndex.from(ballIndex), BallNumber.from(numbers.get(ballIndex - ONE))))
+                .toList();
+        return new Balls(balls);
+    }
+
+}

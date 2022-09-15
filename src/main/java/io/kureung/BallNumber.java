ecee1c622a78c3eeb70c0059a9104df17b4b518d
kureungkureung <terry9611@naver.com>
2022-09-15 20:13:04 +0900

    feat: 공 숫자에 숫자 정보 정의

--- src/main/java/io/kureung/BallNumber.java
+++ src/main/java/io/kureung/BallNumber.java
@@ -1,13 +1,23 @@
 package io.kureung;
 
 public enum BallNumber {
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE;
+    ONE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9);
+
+    private final int number;
+
+    BallNumber(int number) {
+        this.number = number;
+    }
+
+    public int number() {
+        return number;
+    }
 }

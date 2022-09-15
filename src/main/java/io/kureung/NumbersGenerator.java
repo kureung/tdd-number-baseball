7662f87f35d61668a2e647fe51bd8490b0e208fc
kureungkureung <terry9611@naver.com>
2022-09-15 19:59:52 +0900

    feat: 랜덤 숫자 생성 구현체 생성 및 랜덤 숫자 생성 기능 구현

--- src/main/java/io/kureung/NumbersGenerator.java
+++ src/main/java/io/kureung/NumbersGenerator.java
@@ -1,9 +1,31 @@
 package io.kureung;
 
 import java.util.List;
+import java.util.Random;
 
 public interface NumbersGenerator {
 
     List<Integer> numbers();
+
+    class RandomNumbersGenerator implements NumbersGenerator {
+
+        private static final int NUMBER_OF_BALLS = 3;
+
+        private static final Random RANDOM = new Random();
+
+        private static final int SMALLEST_NUMBER_OF_BALL = 1;
+
+        private static final int BIGGEST_NUMBER_OF_BALL = 9;
+
+        @Override
+        public List<Integer> numbers() {
+            return RANDOM.ints(SMALLEST_NUMBER_OF_BALL, BIGGEST_NUMBER_OF_BALL + SMALLEST_NUMBER_OF_BALL)
+                    .distinct()
+                    .limit(NUMBER_OF_BALLS)
+                    .boxed()
+                    .toList();
+        }
+
+    }
 
 }

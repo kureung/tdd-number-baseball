5cc97b32ff90603ccf2c2501bf7af48021ad5da2
kureungkureung <terry9611@naver.com>
2022-09-15 23:35:46 +0900

    feat: 공 리스트의 첫 번째 공 반환 테스트 구현

--- src/test/java/io/kureung/BallsTest.java
+++ src/test/java/io/kureung/BallsTest.java
@@ -2,7 +2,9 @@ package io.kureung;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import io.kureung.NumbersGenerator.FakeNumbersGenerator;
 import io.kureung.NumbersGenerator.RandomNumbersGenerator;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class BallsTest {
@@ -12,6 +14,13 @@ class BallsTest {
         NumbersGenerator numbersGenerator = new RandomNumbersGenerator();
         assertThatCode(() -> Balls.from(numbersGenerator))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    void 첫_번째_볼을_반환한다() {
+        NumbersGenerator numbersGenerator = new FakeNumbersGenerator(List.of(1, 2, 3));
+        Balls balls = Balls.from(numbersGenerator);
+        assertThat(balls.firstBall()).isEqualTo(new Ball(BallIndex.ONE, BallNumber.ONE));
     }
 
 }

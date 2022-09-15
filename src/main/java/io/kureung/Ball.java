9b30b22f0658b779b28691f95f5a4e3785b8aee2
kureungkureung <terry9611@naver.com>
2022-09-15 23:37:01 +0900

    feat: 공의 동등성 비교 기능 추가

--- src/main/java/io/kureung/Ball.java
+++ src/main/java/io/kureung/Ball.java
@@ -1,5 +1,7 @@
 package io.kureung;
 
+import java.util.Objects;
+
 public class Ball {
 
     private final BallIndex ballIndex;
@@ -8,6 +10,19 @@ public class Ball {
     public Ball(BallIndex ballIndex, BallNumber ballNumber) {
         this.ballIndex = ballIndex;
         this.ballNumber = ballNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return ballIndex == ball.ballIndex && ballNumber == ball.ballNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballIndex, ballNumber);
     }
 
 }

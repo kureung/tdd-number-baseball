95b9c835199e5e7646d5ce34e3843309b54c5b85
kureungkureung <terry9611@naver.com>
2022-09-15 20:26:22 +0900

    feat: 공 생성

---
+++ src/main/java/io/kureung/Ball.java
@@ -0,0 +1,13 @@
+package io.kureung;
+
+public class Ball {
+
+    private final BallIndex ballIndex;
+    private final BallNumber ballNumber;
+
+    public Ball(BallIndex ballIndex, BallNumber ballNumber) {
+        this.ballIndex = ballIndex;
+        this.ballNumber = ballNumber;
+    }
+
+}

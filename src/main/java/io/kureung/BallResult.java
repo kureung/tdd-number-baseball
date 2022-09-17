da6f8bec69461e87f81a3141b4b744086a755f8b
kureungkureung <terry9611@naver.com>
2022-09-17 11:39:18 +0900

    feat : 볼 결과 객체 구현

---
+++ src/main/java/io/kureung/BallResult.java
@@ -0,0 +1,30 @@
+package io.kureung;
+
+import java.util.Objects;
+
+public class BallResult {
+
+    private final int ball;
+
+    public BallResult(int ball) {
+        this.ball = ball;
+    }
+
+    public int ball() {
+        return ball;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BallResult that = (BallResult) o;
+        return ball == that.ball;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ball);
+    }
+
+}

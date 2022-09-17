8a372d58935d149f84e7863ed9352daa7639ad45
kureungkureung <terry9611@naver.com>
2022-09-17 11:49:59 +0900

    feat : 볼 결과의 더하기 테스트 구현

---
+++ src/test/java/io/kureung/BallResultTest.java
@@ -0,0 +1,18 @@
+package io.kureung;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BallResultTest {
+
+    @Test
+    void 볼_결과는_더할_수_있다() {
+        BallResult oneBall = new BallResult(1);
+        BallResult twoBall = new BallResult(2);
+
+        BallResult threeBall = oneBall.addedBall(twoBall);
+        assertThat(threeBall).isEqualTo(new BallResult(3));
+    }
+
+}

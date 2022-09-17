b15f2481a9570b6f4a242020207d9deb4e9022be
kureungkureung <terry9611@naver.com>
2022-09-18 08:34:03 +0900

    feat: 스트라이크 결과의 더하기 기능 구현

--- src/main/java/io/kureung/StrikeResult.java
+++ src/main/java/io/kureung/StrikeResult.java
@@ -1,11 +1,30 @@
 package io.kureung;
 
+import java.util.Objects;
+
 public class StrikeResult {
 
     private final int strike;
 
     public StrikeResult(int strike) {
         this.strike = strike;
+    }
+
+    public StrikeResult addedStrike(StrikeResult twoStrike) {
+        return new StrikeResult(this.strike + twoStrike.strike);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StrikeResult that = (StrikeResult) o;
+        return strike == that.strike;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(strike);
     }
 
 }
--- src/test/java/io/kureung/StrikeResultTest.java
+++ src/test/java/io/kureung/StrikeResultTest.java
@@ -1,6 +1,7 @@
 package io.kureung;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class StrikeResultTest {
@@ -10,7 +11,7 @@ class StrikeResultTest {
         StrikeResult oneStrike = new StrikeResult(1);
         StrikeResult twoStrike = new StrikeResult(2);
         StrikeResult threeStrike = oneStrike.addedStrike(twoStrike);
-        Assertions.assertThat(threeStrike).isEqualTo(new StrikeResult(3));
+        assertThat(threeStrike).isEqualTo(new StrikeResult(3));
     }
 
 }

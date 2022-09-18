1d11fd6a598a46157adeb9047bea679ca2def391
kureungkureung <terry9611@naver.com>
2022-09-18 11:10:51 +0900

    feat: 숫자 야구 통합 판별 테스트

---
+++ src/test/java/io/kureung/JudgmentTest.java
@@ -0,0 +1,33 @@
+package io.kureung;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import io.kureung.NumbersGenerator.FakeNumbersGenerator;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class JudgmentTest {
+
+    @Test
+    void 숫자_야구_통합_판별() {
+        Balls computerBalls = balls(List.of(1, 2, 3));
+        Balls userBalls = balls(List.of(3, 2, 1));
+
+        Player computer = new ComputerPlayer(computerBalls);
+        Player user = new UserPlayer(userBalls);
+
+        Judgment judgment = new Judgment(computer, user);
+        JudgmentResult judgmentResult = judgment.result();
+
+        assertAll(
+                () -> assertThat(judgmentResult.strike()).isEqualTo(1),
+                () -> assertThat(judgmentResult.ball()).isEqualTo(2)
+        );
+    }
+
+    private Balls balls(List<Integer> numbers) {
+        NumbersGenerator computerNumbers = new FakeNumbersGenerator(numbers);
+        return Balls.from(computerNumbers);
+    }
+
+}

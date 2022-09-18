5486e306ab68946b54762f4f006b324e71f176ec
kureungkureung <terry9611@naver.com>
2022-09-18 10:32:06 +0900

    feat: 유저 플레이어의 공 반환 테스트

---
+++ src/test/java/io/kureung/UserPlayerTest.java
@@ -0,0 +1,22 @@
+package io.kureung;
+
+import io.kureung.NumbersGenerator.FakeNumbersGenerator;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UserPlayerTest {
+
+    @Test
+    void 유저_플레이어는_공들을_반환할_수_있다() {
+        Balls balls = balls();
+        Player player = new UserPlayer(balls);
+        Assertions.assertThat(player.balls()).isEqualTo(balls);
+    }
+
+    private Balls balls() {
+        NumbersGenerator numbersGenerator = new FakeNumbersGenerator(List.of(1, 2, 3));
+        return Balls.from(numbersGenerator);
+    }
+
+}

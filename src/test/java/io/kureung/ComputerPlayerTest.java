71bafbb1b5129f5c9d9d390ca77523c529e76c40
kureungkureung <terry9611@naver.com>
2022-09-18 10:22:18 +0900

    feat: 컴퓨터 공 반환 테스트

---
+++ src/test/java/io/kureung/ComputerPlayerTest.java
@@ -0,0 +1,22 @@
+package io.kureung;
+
+import io.kureung.NumbersGenerator.FakeNumbersGenerator;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ComputerPlayerTest {
+
+    @Test
+    void 컴퓨터_플레이어는_공들을_반환할_수_있다() {
+        Balls balls = balls();
+        Player player = new ComputerPlayer(balls);
+        Assertions.assertThat(player.balls()).isEqulato(balls);
+    }
+
+    private Balls balls() {
+        NumbersGenerator numbersGenerator = new FakeNumbersGenerator(List.of(1, 2, 3));
+        return Balls.from(numbersGenerator);
+    }
+
+}
